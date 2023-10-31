54f28e5a6e0d648f0f6f40f6c2944ab9e30ff84f
Pranav Jani <41084323+pranav1311@users.noreply.github.com>
2023-11-01 00:16:29 +0530

    Update UsingLoops3.java

--- UsingLoops3.java
+++ UsingLoops3.java
@@ -15,31 +15,25 @@
         Scanner sc = new Scanner (System.in);
         
             // PUT THE CODE BELOW IN A DO WHILE LOOP
-        do
+        while (true)
         {
             System.out.print("\n" + "Enter length of square: ");
             length = sc.nextDouble();
             System.out.println("Perimeter = " + (4*length));
-            System.out.print("Do you wish to enter another value? [y/n]: ");
-            yn = sc.next().charAt(0);
             
-            boolean notValid = true;
-            do
-            {
-                if (yn == 'y' || yn == 'Y' || yn == 'n' || yn == 'N')
-                {
-                    notValid = false;
-                }
-                else
-                {
-                    System.out.println("INVALID INPUT!");
-                    System.out.print("Do you wish to enter another value? [y/n]: ");
-                    yn = sc.next().charAt(0);
-                }
-            }while(notValid);
-        }while(yn=='y' || yn=='Y');
+            while(true) {
+		 System.out.print("Do you wish to enter another value? [y/n]: ");
+		 yn = sc.next().charAt(0).toLowerCase();
+		 if (yn =='y') {
+                     break;
+                 } else if(yn =='n) {
+                     return;
+                 }else {
+                   System.out.println("INVALID INPUT!");
+                 }
+            }
+        }
 
-        // WRITE CODE TO ASK IF USER WISHES TO RE-RUN
         System.out.println("\n" + "--THE PROGRAM HAS ENDED--");
         sc.close();
     }
