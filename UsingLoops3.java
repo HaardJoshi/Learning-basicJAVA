c42e1529bd9a86641bd87448ef90001cd8ad6cd5
Pranav Jani <41084323+pranav1311@users.noreply.github.com>
2023-11-01 00:23:41 +0530

    Tested and working.

--- UsingLoops3.java
+++ UsingLoops3.java
@@ -11,7 +11,7 @@
      public static void main(String args[]) 
     {
         double length;
-        char yn;
+        Character yn;
         Scanner sc = new Scanner (System.in);
         
             // PUT THE CODE BELOW IN A DO WHILE LOOP
@@ -23,10 +23,13 @@
             
             while(true) {
 		 System.out.print("Do you wish to enter another value? [y/n]: ");
-		 yn = sc.next().charAt(0).toLowerCase();
+		 yn = sc.next().charAt(0);
+		 yn = Character.toLowerCase(yn);
 		 if (yn =='y') {
                      break;
                  } else if(yn =='n') {
+                             System.out.println("\n" + "--THE PROGRAM HAS ENDED--");
+                     sc.close();
                      return;
                  }else {
                    System.out.println("INVALID INPUT!");
@@ -34,7 +37,6 @@
             }
         }
 
-        System.out.println("\n" + "--THE PROGRAM HAS ENDED--");
-        sc.close();
+        
     }
  }
